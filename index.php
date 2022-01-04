ae90e52fbf5c815c63251c1eb485ff6db1ab13e8
PhamQuocTri <hipsport792001@gmail.com>
2022-01-04 23:51:28 +0700

    signup

--- index.php
+++ index.php
@@ -1,74 +1,83 @@
+<?php
+session_start();
+if (isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+}
+?>
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Login</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/styles.css">
-    </head>
-    <body>
-        <main id="login">
-            <div class="login__column">
-                <img src="images/phoneImage.png" class="login__phone" />
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Sign Up</title>
+</head>
+
+<body>
+    <main id="sign-up">
+        <div class="sign-up__column">
+
+            <!--  -->
+            <div class="sign-up__box">
+                <img src="images/loginLogo.png" class="sign-up__logo" />
+                <h2>Sign up to see photos and videos from your friends.</h2>
+                <button>Log in with Facebook</button>
+                <span class="sign-up__divider">or</span>
+                <form action="#" method="POST" enctype="multipart/form-data" class="sign-up__form" autocomplete="on">
+                    <div class="error"></div>
+                    <input type="text" name="email" placeholder="Email" required autocomplete="off" />
+                    <input type="text" name="fullname" placeholder="Full name" required autocomplete="off">
+                    <input type="text" name="username" placeholder="User name" required autocomplete="off">
+                    <input type="password" name="password" placeholder="Password" required autocomplete="off" />
+                    <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+                    <div class="button__signup"><input type="submit" name="submit" value="Sign up"></div>
+                </form>
+                <p>By signing up, you agree to our Terms , Data Policy and Cookies Policy .</p>
             </div>
-            <div class="login__column">
-                <div class="login__box">
-                    <img src="images/loginLogo.png" class="login__logo" />
-                    <!--  -->
-                    <form action="process-login.php" method="post" class="login__form">
-                        <input type="text" name="txtUsername" placeholder="Username" required />
-                        <input type="password" name="txtPassword" placeholder="Password" required />
-                        <input type="submit" name="loginBtn" value="Log in" />
-                    </form>
-                    <!--  -->
-                    <span class="login__divider">or</span>
-                    <a href="#" class="login__link">
-                        <i class="fa fa-money"></i>
-                        Log in with Facebook
-                    </a>
-                    <!-- Hiển thị thông báo lỗi vì nhập sai tk or mk -->
-                    <?php 
-                        if (isset ($_GET['error'])) {
-                            echo "<h5 style='color:red'> {$_GET['error']} <h5/>";
-                        }
-                    ?>
-                    <!--  -->
-                    <a href="#" class="login__link login__link--small">Forgot password</a>
-                </div>
-                <div class="login__box">
-                    <span>Don't have an account?</span> <a href="#">Sign up</a>
-                </div>
-                <div class="login__box--transparent">
-                    <span>Get the app.</span>
-                    <div class="login__appstores">
-                        <img src="images/ios.png" class="login__appstore" alt="Apple appstore logo" title="Apple appstore logo" />
-                        <img src="images/android.png" class="login__appstore" alt="Android appstore logo" title="Android appstore logo" />
-                    </div>
+            <!--  -->
+
+            <!--  -->
+            <div class="sign-up__box">
+                <span>Have an account?</span> <a href="./login.php">Log in</a>
+            </div>
+            <!--  -->
+
+            <div class="sign-up__box--transparent">
+                <span>Get the app.</span>
+                <div class="sign-up__appstores">
+                    <img src="images/ios.png" class="sign-up__appstore" alt="Apple appstore logo" title="Apple appstore logo" />
+                    <img src="images/android.png" class="sign-up__appstore" alt="Android appstore logo" title="Android appstore logo" />
                 </div>
             </div>
-        </main>
-        <footer class="footer">
-            <div class="footer__column">
-                <nav class="footer__nav">
-                    <ul class="footer__list">
-                        <li class="footer__list-item"><a href="#" class="footer__link">About Us</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Support</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Blog</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Press</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Api</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Jobs</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Privacy</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Terms</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Directory</a></li>
-                        <li class="footer__list-item"><a href="#" class="footer__link">Language</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="footer__column">
-                <span class="footer__copyright">© 2021 meta</span>
-            </div>
-        </footer>
-    </body>
+        </div>
+    </main>
+    <footer class="footer">
+        <div class="footer__column">
+            <nav class="footer__nav">
+                <ul class="footer__list">
+                    <li class="footer__list-item"><a href="#" class="footer__link">About Us</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Support</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Blog</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Press</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Api</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Jobs</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Privacy</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Terms</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Directory</a></li>
+                    <li class="footer__list-item"><a href="#" class="footer__link">Language</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="footer__column">
+            <span class="footer__copyright">© 2021 Instagram from Meta</span>
+        </div>
+    </footer>
+
+    <script src="JS/signup.js"></script>
+
+</body>
+
 </html>
