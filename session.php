f96315815c4d4f91971deaa24dbe64b0ab71a924
Qa160601 <luonyeuvo2001@gmail.com>
2022-01-14 21:38:02 +0700

    fix post

--- session.php
+++ session.php
@@ -9,7 +9,4 @@ $id = $_SESSION['unique_id'];
 
 $query=mysqli_query ($conn,"SELECT * FROM users WHERE user_id ='$id'");
 $row=mysqli_fetch_array($query);
-$profile_picture=$row['img'];
-$fullname=$row['fullname'];
-$username=$row['username'];
 ?>
