0f710bd0011449b2229193b786a79fde7fa58756
Qa160601 <luonyeuvo2001@gmail.com>
2022-01-14 20:37:55 +0700

    fix php

--- php/insert-chat.php
+++ php/insert-chat.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
     session_start();
     if(isset($_SESSION['unique_id'])){
@@ -13,26 +12,4 @@
     }else{
         header("location: ../inbox.php");
     }
-
-
-    
-=======
-<?php 
-    session_start();
-    if(isset($_SESSION['unique_id'])){
-        include_once "config.php";
-        $outgoing_id = $_SESSION['unique_id'];
-        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
-        $message = mysqli_real_escape_string($conn, $_POST['message']);
-        if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
-                                        VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
-        }
-    }else{
-        header("location: ../inbox.php");
-    }
-
-
-    
->>>>>>> master
 ?>
